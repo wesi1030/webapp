b5e3a98c7e3a52009c2d5199ebe321725a732986
wesi1030 <51756770+wesi1030@users.noreply.github.com>
2019-06-12 19:15:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hallo Welt! from main';
+
+var myAlert = document.querySelector('p');
+myAlert.textContent ='haslllo ';
+
+
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle");
+    element.textContent = 'test content'
+  } 
